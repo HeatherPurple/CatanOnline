53fadfea6cf2447359ef0ac9dc3098f8211878f8
Stepan Lutoshkin <62564571+S1lentSt0rm@users.noreply.github.com>
2024-01-11 15:35:19 +0300

    resolving conflicts

--- Assets/Scripts/TestingGrid.cs
+++ Assets/Scripts/TestingGrid.cs
@@ -1,18 +1,4 @@
-<<<<<<< Updated upstream
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class TestingGrid : MonoBehaviour {
-    
-    [SerializeField] private GameObject pointPrefab;
-    [SerializeField] private int mapSize;
-    void Start() {
-        HexGrid hexGrid = new HexGrid(10, pointPrefab, mapSize);
-        
-    }
-
-=======
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -29,5 +15,4 @@ public class TestingGrid : MonoBehaviour {
             Debug.Log("hit");
         }
     }
->>>>>>> Stashed changes
 }
