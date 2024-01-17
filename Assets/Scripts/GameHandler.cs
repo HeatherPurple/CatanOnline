18077606453f0f3f95d05696af7ae1a9476b2a42
Stepan Lutoshkin <62564571+S1lentSt0rm@users.noreply.github.com>
2024-01-17 12:05:21 +0300

    GameHandler update + gitignore update

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -3,10 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameHandler : MonoBehaviour {
-
-   // public static GameHandler Instance { get; private set; }
-   
+public static class GameHandler {
+    
     public static Action OnGameStateChanged;
     
     public enum GameState {
@@ -20,12 +18,8 @@ public class GameHandler : MonoBehaviour {
     // dice roll => (getting cards) / (losing cards -> bandits turn -> stealing a card) => construction phase
     
     private static GameState currentState = GameState.BuildingGameField;
-    
-    // private void Awake() {
-    //     //Instance = this;
-    //     
-    //     currentState = GameState.BuildingGameField;
-    // }
+
+    public static GameState GetCurrentGameState() => currentState;
 
     public static void ChangeGameState(GameState newState) {
         currentState = newState;
